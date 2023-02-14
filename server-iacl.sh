cca35f893b46655c7fd44c3aa5b556a003ef498c
Gabriel Joshua <gjoshuacalixto@outlook.com>
2023-02-14 20:43:46 -0300

    Update server-iacl.sh

--- server-iacl.sh
+++ server-iacl.sh
@@ -10,6 +10,6 @@ apt-get install unzip -y
 echo "Baixando a Aplicação"
 cd /tmp
 wget https://github.com/GabrielJ0shua/GabrielJ0shua.github.io/archive/refs/heads/master.zip
-unzip main.zip
-cd linux-site-dio-main
+unzip GabrielJ0shua.github.io-master.zip
+cd GabrielJ0shua.github.io-master
 cp -R * /var/www/html/
